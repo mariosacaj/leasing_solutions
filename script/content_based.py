c22657880670f0e82b718d5294a6de3fed0dd8eb
mimmi <mimmi.ferrini@gmail.com>
2020-05-16 16:24:16 +0200

    updated rating in content based

--- script/content_based.py
+++ script/content_based.py
@@ -14,7 +14,7 @@ def Sort(sub_li):
 df = load_data.load_data()
 df.fillna(0, inplace=True)
 df = shuffle(df).reset_index(drop=True)
-df = df.truncate(before=0, after=300)
+df = df.truncate(before=0, after=10000)
 
 Y = df['target'].tolist()
 df1 = df.drop(['target'],axis=1)
@@ -49,19 +49,43 @@ def predict(item, x_train, y_train, KNN):
     total = zeros + ones
     zero_prob = zeros / total
     one_prob = 1 - zero_prob
-    if one_prob > zero_prob:
+    if one_prob >= zero_prob:
+        return 1
+    else:
+        return 0
+
+def predict2(item, x_train, y_train, KNN):
+    similarities = compute_similarity(item, x_train)
+    sortedSimilarities = Sort(similarities)
+    topK = sortedSimilarities[:KNN]
+    zeros = 0
+    ones = 0
+    for i in range(len(topK)):
+        index = topK[i][0]
+        target = y_train[index]
+        if target == 0:
+            zeros += (KNN - i)
+        else:
+            ones += (KNN - i)
+    total = zeros + ones
+    zero_prob = zeros / total
+    one_prob = 1 - zero_prob
+    if one_prob >= zero_prob:
         return 1
     else:
         return 0
 
 def accuracy(x_train, x_test, y_train, y_test, KNN):
-    score = 0
+    score1 = 0
+    score2 = 0
     total = len(y_test)
     for i in range(len(x_test)):
         print("PROCESSING ITEM No: " + str(i))
         if predict(x_test[i], x_train, y_train, KNN) == y_test[i]:
-            score += 1
-    return score / total
+            score1 += 1
+        if predict2(x_test[i], x_train, y_train, KNN) == y_test[i]:
+            score2 += 1
+    return score1/total, score2/total
 
 print(accuracy(x_train, x_test, y_train, y_test, 10))
 
